4528f575a808bf849fe536fe214f4902a008956e
DaveCorleto <davide.corleto@fastwebnet.it>
2023-11-23 12:57:45 +0100

    end exercises

--- js/script.js
+++ js/script.js
@@ -21,22 +21,54 @@
 // L’utente inserisce due parole in successione, con due prompt.
 // Il software stampa prima la parola più corta, poi la parola più lunga. 
 
-const firstWord = prompt('Inserisci la prima parola');
-const secondWord = prompt('Inserisci la seconda parola');
+// const firstWord = prompt('Inserisci la prima parola');
+// const secondWord = prompt('Inserisci la seconda parola');
 
-let wordWinner;
-let wordLoser;
+// let wordWinner;
+// let wordLoser;
 
-if (firstWord.length > secondWord.length) {
-    wordWinner = firstWord;
-    wordLoser = secondWord;
-} else if (secondWord.length > firstWord.length) {
-    wordWinner = secondWord;
-    wordLoser = firstWord;
-} else {
-    wordWinner = "Le parole sono lunghe uguali";
-    wordLoser = "Le parole sono lunghe uguali";
+// if (firstWord.length > secondWord.length) {
+//     wordWinner = firstWord;
+//     wordLoser = secondWord;
+// } else if (secondWord.length > firstWord.length) {
+//     wordWinner = secondWord;
+//     wordLoser = firstWord;
+// } else {
+//     alert("Le parole sono lunghe uguali")
+// }
+
+// console.log(wordWinner + wordLoser);
+
+
+
+// let somma = 0;
+
+// for (let i = 1; i <= 5; i++) {
+//   const numero = parseInt(prompt(`Inserisci il ${i}° numero`));
+//   somma += numero;
+// }
+
+// console.log(somma)
+
+
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+
+// Array vuoto 
+
+let list = [];
+
+// chiedi per 6 volte all'utente di inserire un numero
+
+for (let i = 1; i <= 6; i++) {
+  const newitem = parseInt(prompt(`Inserisci il ${i}° numero`));
+
+  if (newitem % 2 !== 0 ){
+    list.push(newitem);
+  }
 }
 
-console.log(wordWinner + wordLoser);
+console.log(list)
+
 
